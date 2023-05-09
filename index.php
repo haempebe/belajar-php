a62964490db840c657f49653ddd9901fefe7c4e6
haempebe <hamidzanpranajanalabhamkerti@example.com>
2023-05-09 09:02:31 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <?php
+    echo "belajar PHP";
+    echo "\n";
+    // tahap 2
+    $name ="belajar php bla bla bla";
+    echo $name;
+    ?>
+</body>
+</html>
